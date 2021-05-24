ec1cc5c0c19687bba33c326895bfb2b69018e969
David Garrett <dave@22ndsolutions.com>
2021-05-23 18:25:18 -0700

    Adding the relative path for the docs generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../..'))
 import heykube
-#sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
