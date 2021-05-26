6fab8c0bd03a6ca1ee817d4ae0cb7361d83c02ad
David Garrett <dave@22ndsolutions.com>
2021-05-25 19:22:55 -0700

    Cleaning up __init__ for the library, adding service_identity to the library

--- heykube/__init__.py
+++ heykube/__init__.py
@@ -1,6 +1,5 @@
 from .heykube_btle import heykube_btle
 from .heykube import heykube
-from .heykube import heykube_admin
 from .heykube import Moves
 from .heykube import Match
 from .heykube import Facelet
